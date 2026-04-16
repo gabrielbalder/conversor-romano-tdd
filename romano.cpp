af419c71f9c9c12d97e6ca0ceb00f9662b477661
Gabriel Balder <gabrielbalder@gmail.com>
2026-04-16 19:43:52 -0300

    codigo 1: implementa conversao e faz passar o teste do I

--- romano.cpp
+++ romano.cpp
@@ -1,6 +1,10 @@
 #include "romano.hpp"
 
 int romano_para_inteiro(std::string romano) {
+    
     // Retornamos 0 de propósito para o teste inicial falhar (Fase RED)
-    return 0; 
+    if (romano == "I") {
+        return 1;
+    }
+    return -1; 
 }
